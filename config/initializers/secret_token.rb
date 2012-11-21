622678937f4b371ed6b84ebc379a48f7933644a4
Caroline <Caroline@TW>
2012-11-21 11:00:03 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DadTheBuilder::Application.config.secret_token = '0e4a81b9f5311d9073f30947c42a6b55a3c0e88aded56e2edc32ff99a92610d60397af58292905031dd294121111e2c611cd2dd07d7e8508a10a93ee66c36e21'
